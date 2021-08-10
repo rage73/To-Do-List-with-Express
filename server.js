515b9a6b40ff5a2977442d0200d003ee4cb08f46
rage73 <raghavagrawal2000ra@gmail.com>
2021-08-10 22:19:21 +0530

    basic

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const express = require('express');
-const app = expres();
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+let tasks = [];
+
+app.get('/', (req,res) => {
+
+    let tasklist = tasks.map(t => `<li>${t}</li>`).join('\n');
+
+    res.send(`
+        <html>
+        <body>
+            <form action="/" method="POST">
+                <input name="newtask">
+                <button type="Submit">ADD</button>
+            </form>
+            <ul>
+                ${tasklist}
+            </ul>
+        </body>
+        </html>
+    `);
+});
+
+app.post('/', (req, res) => {
+    tasks.push(req.body.newtask);
+    res.redirect('/');
+});
 
 app.listen(5555,() => {
     console.log('server started at http://localhost:5555');
